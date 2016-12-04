dad7c6795dc9b2248b5bb4665d0da3b39f4b1fc7
mosavy <www.mortezamosavy94@gmail.com>
2016-12-04 16:15:33 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -260,7 +260,7 @@ function create_config( )
       "youtube",
       "media_handler",
     "moderation"},
-    sudo_users = {our_id},
+    sudo_users = {187807271},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
