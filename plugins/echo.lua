8c8c3ea31cfc2bf028bfb56b51b9d0dbbc7cc1bc
mosavy <www.mortezamosavy94@gmail.com>
2017-02-02 01:40:57 +0330

    Update echo.lua

--- plugins/echo.lua
+++ plugins/echo.lua
@@ -1,3 +1,31 @@
+local function pre_process(msg)
+local to = msg.to.type
+local service = msg.service
+	if to == 'user' and msg.fwd_from then
+		local user = 'user#id'..msg.from.id
+		local from_id = msg.fwd_from.peer_id
+		if msg.fwd_from.first_name then
+			from_first_name = msg.fwd_from.first_name:gsub("_", " ")
+		else
+			from_first_name = "None"
+		end
+		if msg.fwd_from.last_name then
+			from_last_name = msg.fwd_from.last_name:gsub("_", " ")
+		else
+			from_last_name = "None"
+		end
+		if msg.fwd_from.username then
+			from_username = "@"..msg.fwd_from.username
+		else
+			from_username = "@[none]"
+		end
+		text = "User From Info:\n\nID: "..from_id.."\nFirst: "..from_first_name.."\nLast: "..from_last_name.."\nUsername: "..from_username
+		send_large_msg(user, text)
+	end
+	return msg
+end
+
+
 local function run(msg, matches)
   local text = matches[1]
   local b = 1
@@ -15,5 +43,6 @@ return {
   patterns = {
     "^!echo +(.+)$"
   },
-  run = run
+  run = run,
+  pre_process = pre_process
 }
